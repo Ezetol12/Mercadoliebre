6c4cdb476fcd35257abe60bdbef2cfe41559d94c
Ezetol12 <ezequiel.tolaba1@gmail.com>
2023-08-08 23:52:22 -0300

    Actualizar package.json y archivos de estilo, app.js y vistas

--- src/app.js
+++ src/app.js
@@ -2,9 +2,8 @@ const express = require("express")
 const app = express()
 const path= require("path")
 app.use(express.static("public")) //linea importante para usar img
-app.listen(3000,()=>{
-  console.log("esta funcionando")
-})
+const port = process.env.PORT || 3001;
+app.listen(port, () => "servidor corriendo  en el  puerto port ");
 app.get ('/',(req, res)=>{
   res.sendFile(path.join(__dirname, "./views/home.html")) //el error fue ponermal la ruta
   // res.sendFile(path.resolve(__dirname,hola.html)
@@ -13,6 +12,6 @@ app.get ('/',(req, res)=>{
 app.get('/registro',(req,res)=>{ //aca va solo el nombre
   res.sendFile(path.join(__dirname, "./views/registro.html")); //aca la ruta entera 
 })
-app.get("/ingresa", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/ingresa.html")); //aca la ruta entera
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html")); //aca la ruta entera
 });
